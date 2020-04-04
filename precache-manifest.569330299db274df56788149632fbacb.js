39880ca516279a42dcfdb488886a3c05fc6ad786
Dmitry Rastorguev <dmitry@rastorguev.co.uk>
2020-04-04 10:36:22 +0100

    Further updatess

---
+++ precache-manifest.569330299db274df56788149632fbacb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0091e7cbe7f8fc0657a37f6cae4b9cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eca2e4d906845cf79aa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6912ac15c14783fd90e",
+    "url": "/static/js/2.f2460396.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2460396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eca2e4d906845cf79aa",
+    "url": "/static/js/main.92fd3795.chunk.js"
+  },
+  {
+    "revision": "9033937442a2065203e5",
+    "url": "/static/js/runtime-main.e03ed2f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
